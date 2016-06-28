4a308b8f6de02bb0357196a595201d16979b5a75
Fannyu Chien <fannyu.chien@yahoo.com>
2016-06-28 16:02:13 +0800

    post reminder added

---
+++ app/src/main/java/fannyu/didyouworkout20/add_reminder.java
@@ -0,0 +1,23 @@
+package fannyu.didyouworkout20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class add_reminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_reminder);
+    }
+
+    //get the text
+    EditText exerciseName;
+    EditText exerciseQuantity;
+
+    public void onAdd()
+    {
+        
+    }
+}
---
+++ app/src/main/java/fannyu/didyouworkout20/post_reminder.java
@@ -0,0 +1,13 @@
+package fannyu.didyouworkout20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class post_reminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_reminder);
+    }
+}

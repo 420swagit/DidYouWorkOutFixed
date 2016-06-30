88404d6fc9d8869cd98e9f6f95fd6d8121325e1c
Fannyu Chien <fannyu.chien@yahoo.com>
2016-06-30 15:47:00 +0800

    Added SQL class

--- app/src/main/java/Data.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by fanny on 6/30/2016.
- */
-public class Data {
-}
---
+++ app/src/main/java/createTable.java
@@ -0,0 +1,39 @@
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+
+public class createTable extends SQLiteOpenHelper {
+
+    public static final String TABLE_COMMENTS = "comments";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_COMMENT = "comment";
+
+    private static final String DATABASE_NAME = "commments.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_CREATE = "create table"
+            +TABLE_COMMENTS + "( " + COLUMN_ID
+            +" integer primary key autoincrement, " + COLUMN_COMMENT
+            +"text not null);";
+
+    public createTable(Context context)
+    {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase database)
+    {
+        database.execSQL(DATABASE_CREATE);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        Log.w(createTable.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_COMMENTS);
+        onCreate(db);
+    }
+
+}

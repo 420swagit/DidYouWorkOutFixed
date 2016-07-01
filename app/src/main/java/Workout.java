f9758f4fa9be5c42566f6b90ad2bb32b6c8c891b
Fannyu Chien <fannyu.chien@yahoo.com>
2016-07-01 09:54:03 +0800

    Workout class added

---
+++ app/src/main/java/Workout.java
@@ -0,0 +1,21 @@
+/**
+ * Created by fanny on 7/1/2016.
+ */
+public class Workout {
+
+    int id;
+    String name;
+    String quantity;
+
+    public Workout()
+    {
+
+    }
+
+    public Workout(int id, String name, String quantity)
+    {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+    }
+}

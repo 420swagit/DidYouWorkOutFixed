7bffb25d4f0303d94a9ef87d03771138fed8989a
Fannyu Chien <fannyu.chien@yahoo.com>
2016-07-12 13:50:47 +0800

    New activities

--- app/src/main/java/fannyu/didyouworkout20/addWorkout.java
+++ app/src/main/java/fannyu/didyouworkout20/addWorkout.java
@@ -6,28 +6,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class addWorkout extends AppCompatActivity {
 
     private Button mButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_workout);
 
-        mButton = (Button)findViewById(R.id.addWorkout);
+        mButton = (Button)findViewById(R.id.previous);
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(v.getContext(), addWorkout.class);
+                Intent i = new Intent(v.getContext(), MainActivity.class);
                 startActivity(i);
             }
         });
-
     }
-
-
-
-
 }

697f324d291ecd0a64903c3ef3176b99eb42d00c
Fannyu Chien <fannyu.chien@yahoo.com>
2016-07-22 15:14:38 +0800

    add buttons

--- app/src/main/java/fannyu/didyouworkout20/viewWorkout.java
+++ app/src/main/java/fannyu/didyouworkout20/viewWorkout.java
@@ -1,13 +1,28 @@
 package fannyu.didyouworkout20;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class viewWorkout extends AppCompatActivity {
+
+    private Button mButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_workout);
+
+        mButton = (Button)findViewById(R.id.previous);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
